f349bbee2d6c52fd683bd882611ad4357f73525d
GubarievaAnna <annagubarieva@gmail.com>
2022-08-02 13:39:07 +0300

    update classes

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,9 +4,7 @@ import s from './FriendListItem.module.css';
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={s.item}>
-      <span
-        className={`${s.status} ${isOnline ? s.statusOnline : s.statusOffline}`}
-      ></span>
+      <span className={isOnline ? s.statusOnline : s.statusOffline}></span>
       <img className={s.avatar} src={avatar} alt={'user ' + name} width="48" />
       <p className={s.name}>{name}</p>
     </li>
